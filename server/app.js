e6f7bff7c6f0dc18a2eb80e11b9a58a8d7b169a4
realroot <taste1729@gmail.com>
2021-09-10 15:33:29 +0900

    feat:setting

---
+++ server/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
+
+const usersRouter = require('./routers/user');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "DELETE", "POST", "PATCH"],
+  })
+);
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Welcome, closet Server!");
+// });
+
+app.use('/user', usersRouter);
+
+
+const HTTPS_PORT = 4000;
+let server;
+
+server = app.listen(HTTPS_PORT, () => {
+  console.log("server 실행");
+});
+
+module.exports = server;
