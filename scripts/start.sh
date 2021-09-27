58a762a4ffd0403efde1eef7a9bbdc0867b16784
realroot <taste1729@gmail.com>
2021-09-27 23:28:58 +0900

    feat:deploy-test1

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,9 @@ export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --name
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
+export ID=$(aws ssm get-parameters --region ap-northeast-2 --names ID --query Parameters[0].Value | sed 's/"//g')
+export PASS=$(aws ssm get-parameters --region ap-northeast-2 --names PASS --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
