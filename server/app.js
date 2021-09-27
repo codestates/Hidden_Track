94759c46d6cbcb6b46d8589e898faf2869e8c0d4
realroot <taste1729@gmail.com>
2021-09-28 00:17:56 +0900

    fix:deploy-test1

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: 'https://www.hiddentrack.link',
     credentials: true,
     methods: ["GET", "DELETE", "POST", "PATCH"],
   })
