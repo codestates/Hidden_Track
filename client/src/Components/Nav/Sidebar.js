2a31511524a371fbd976212843a616ed28629dcb
james940522 <James940522@gmail.com>
2021-09-30 12:02:42 +0900

    fix sidebar

--- client/src/Components/Nav/Sidebar.js
+++ client/src/Components/Nav/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { inputPlayList, deleteMusic } from '../../Redux/actions/actions';
+import { inputPlayList, deleteMusic, isLoginModalOpenHandler } from '../../Redux/actions/actions';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import PlayList from '../PlayList';
@@ -12,22 +12,29 @@ import active_shuffle from '../../assets/shuffle.png';
 axios.defaults.withCredentials = true;
 const default_album_img = 'https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/%EC%95%A8%EB%B2%94+img/default_album_img.png';
 
-function Sidebar ({ isSidebarOpen, showSidebar }) {
+function Sidebar ({ isSidebarOpen, showSidebar, handleNotice }) {
   const audio = useRef();
   const isLogin = useSelector(state => state.isLoginReducer.isLogin);
   const { accessToken } = useSelector(state => state.accessTokenReducer);
   const playList = useSelector(state => state.playListReducer.playList);
   console.log('사이드바 플레이리스트', playList);
   const dispatch = useDispatch();
-  // console.log('사이드바 오픈',isSidebarOpen)
-  // console.log('사이드바 로그인 상태',isLogin)
-  // console.log('사이드바',playList);
+
   // state 선언 crrentMusic-현재 재생곡 정보(객체), isRandom-랜덤 확인(불린), previousMusic-이전 곡 인덱스값(배열)
   const [crrentMusic, setCrrentMusic] = useState(playList[playList.length - 1]);
   const [isRandom, setIsRandom] = useState(false);
   const [previousMusic, setPreviousMusic] = useState([]);
-  // console.log(playList);
+  const [afterRender, setAfterRender] = useState(false);
+
   useEffect(() => {
+    audio.current.audio.current.onplay = () => {
+      play1min();
+    };
+
+    audio.current.audio.current.onpause = () => {
+      play1min();
+    };
+
     if (isLogin) {
       axios.get(`${process.env.REACT_APP_API_URL}/playlist`, { headers: { accesstoken: accessToken } })
         .then(res => {
@@ -35,7 +42,8 @@ function Sidebar ({ isSidebarOpen, showSidebar }) {
             // console.log(res.data);
             console.log('플레이리스트', res.data);
             dispatch(inputPlayList(res.data.playlist));
-            // setCrrentMusic(playList[res.data.playlist.length - 1]);
+            setCrrentMusic(res.data.playlist[res.data.playlist.length - 1]);
+            setAfterRender(true);
             // audio.current.pause();
           }
         })
@@ -52,12 +60,57 @@ function Sidebar ({ isSidebarOpen, showSidebar }) {
     }
   }, [isLogin]);
 
-  // useEffect(() => {
-  //   setCrrentMusic(playList[playList.length - 1]);
-  // }, [playList]);
+  let tic = 0;
+  let time;
 
-  function handleChangeMusic (index) {
-    setCrrentMusic(playList[index]);
+  // 비로그인 상태일때 음원 1분재생 해주는 함수
+  function play1min () {
+    if (!isLogin) {
+      console.log(audio.current);
+      console.log(audio.current.audio.current);
+      if (!audio.current.audio.current.paused) {
+        time = setInterval(tictok, 1000);
+      } else {
+        clearInterval(time);
+      }
+    } else {
+      console.log('로그인상태');
+    }
+  }
+
+  function tictok () {
+    console.log(tic);
+    tic += 1;
+    check();
+  }
+
+  function check () {
+    if (tic > 59) {
+      audio.current.audio.current.pause();
+      audio.current.audio.current.currentTime = 0;
+      clearInterval(time);
+      tic = 0;
+      // dispatch(isLoginModalOpenHandler(true))
+      handleNotice('현재 1분 미리듣기 상태입니다. 로그인하시겠어요?', 5000);
+    }
+  }
+
+  function handleChangeMusic (index, key) {
+    if (!isLogin) {
+      // 비로그인 상태에서는 노래 재생중엔 노래 변경 불가능
+      if (audio.current.audio.current.currentTime === 0) {
+        clearInterval(time);
+        tic = 0;
+        handlePreviousMusic('push', playList[playList.indexOf(crrentMusic)]);
+        setCrrentMusic(playList[index]);
+
+        // console.log(audio.current.audio.current.play)
+      }
+    } else {
+      setCrrentMusic(playList[index]);
+      audio.current.audio.current.play();
+      // console.log(audio.current.audio.current.paused)
+    }
   }
 
   // 랜덤 인덱스 생성 함수
@@ -144,28 +197,36 @@ function Sidebar ({ isSidebarOpen, showSidebar }) {
             </button>
           </div>
         </div>
-        <div className='audio'>
+        <div className={isLogin ? 'nfdsafsdaf' : 'min-play'}>
           <AudioPlayer
-            id='sidebar-audio'
+            // className={}
             ref={audio}
             src={crrentMusic ? crrentMusic.soundTrack : ''}
-            controls
+            // handleKeyDown={()=>{console.log('어허')}}
+            // isLogin?controls:''
             volume={0.1}
             autoPlay={false}
-            showSkipControls
+            showJumpControls={!!isLogin}
+            showSkipControls={!!isLogin}
+            hasDefaultKeyBindings={!!isLogin}
+            autoPlayAfterSrcChange={afterRender}
             onEnded={() => {
               if (playList.length <= 1) return;
+              handlePreviousMusic('push', crrentMusic);
               if (!isRandom) {
                 if (isValid('playList', playList.indexOf(crrentMusic) + 1)) {
                   handleChangeMusic(playList.indexOf(crrentMusic) + 1);
+                } else {
+                  handleChangeMusic(playList.indexOf(playList.length - 1));
                 }
               } else {
-                handlePreviousMusic('push', crrentMusic);
                 handleChangeMusic(getRandomNumber(0, playList.length - 1));
               }
             }}
+
             onClickNext={() => {
               if (playList.length <= 1) return;
+              handlePreviousMusic('push', crrentMusic);
               if (!isRandom) {
                 if (isValid('playList', playList.indexOf(crrentMusic) + 1)) {
                   handleChangeMusic(playList.indexOf(crrentMusic) + 1);
@@ -173,34 +234,40 @@ function Sidebar ({ isSidebarOpen, showSidebar }) {
                   handleChangeMusic(0);
                 }
               } else {
-                handlePreviousMusic('push', crrentMusic);
                 handleChangeMusic(getRandomNumber(0, playList.length - 1));
               }
             }}
+
             onClickPrevious={() => {
               if (playList.length <= 1) return;
-              if (!isRandom) {
-                if (isValid('playList', playList.indexOf(crrentMusic) - 1)) {
-                  handleChangeMusic(playList.indexOf(crrentMusic) - 1);
+              if (!previousMusic.length) {
+                if (!isRandom) {
+                  if (isValid('playList', playList.indexOf(crrentMusic) - 1)) {
+                    handleChangeMusic(playList.indexOf(crrentMusic) - 1);
+                  } else {
+                    handleChangeMusic(playList.length - 1);
+                  }
                 } else {
-                  handleChangeMusic(playList.length - 1);
-                }
-              } else {
-                if (!previousMusic.length) {
                   console.log('랜덤-이전곡 없음');
                   handleChangeMusic(getRandomNumber(0, playList.length - 1));
-                } else {
-                  console.log('랜덤-이전곡 있음');
-                  handleChangeMusic(playList.indexOf(previousMusic[previousMusic.length - 1]));
-                  handlePreviousMusic('pop');
                 }
+              } else {
+                console.log('이전곡 있음');
+                handleChangeMusic(playList.indexOf(previousMusic[previousMusic.length - 1]));
+                handlePreviousMusic('pop');
               }
             }}
+
           />
         </div>
       </div>
       <div className='sidebar-play-list-box'>
-        <ul className='sidebar-play-ul'>
+        <ul
+          className='sidebar-play-ul' onClick={() => {
+            clearInterval(time);
+            tic = 0;
+          }}
+        >
           {playList.length
             ? playList.map((el, idx) => {
               return (
@@ -210,6 +277,7 @@ function Sidebar ({ isSidebarOpen, showSidebar }) {
                   num={idx}
                   music={el}
                   handleChangeMusic={handleChangeMusic}
+                  handlePreviousMusic={handlePreviousMusic}
                   handleDeleteMusic={handleDeleteMusic}
                 />
               );
