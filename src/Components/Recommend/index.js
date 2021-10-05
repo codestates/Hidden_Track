87e0e280d9df3311fc9243d643fa35258e3919fd
Kangjoohyun <noonssub1004@gmail.com>
2021-10-05 13:05:59 +0900

    test

--- src/Components/Recommend/index.js
+++ src/Components/Recommend/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useSelector} from 'react-redux';
 import {useHistory } from 'react-router';
 
-
 import styled from 'styled-components';
 import axios from 'axios';
 
