cb40e02dfa50ce680134077a82c2dceffd5f3d3f
Kangjoohyun <noonssub1004@gmail.com>
2021-10-07 20:53:10 +0900

    modi: Landing, Main Component

--- client/src/Pages/Main/index.js
+++ client/src/Pages/Main/index.js
@@ -31,14 +31,11 @@ function Main () {
 
   // }, []);
 
-  function moveLanding () {
-    history.push('/landing');
-  }
+
   return (
 
     <div id='main'>
 
-      <button onClick={moveLanding}>랜딩</button>
       <h1 className='main-h1'>Welcome to HIDDEN TRACK!!</h1>
 
       <div className='main-slides'>
